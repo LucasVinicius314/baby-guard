debadd88c79ad364073ded6a605a31898db710e4
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-02-25 18:31:32 -0300

    Validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,30 @@
+import express, { NextFunction, Request, Response } from 'express'
+
 import { detectionRouter } from './routers/detection-router'
-import express from 'express'
+import { json } from 'body-parser'
 
 const port = process.env.PORT
 
 const app = express()
 
+app.use(json())
+
 app.use((req, res, next) => {
   const now = new Date()
 
   console.info(`${now.toISOString()} ${req.method} ${req.url}`)
+  console.info(req.body)
 
   next()
 })
 
 app.use('/api/v1', detectionRouter)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+
+  res.status(500).send()
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
--- server/src/routers/detection-router.ts
+++ server/src/routers/detection-router.ts
@@ -1,7 +1,21 @@
+import * as yup from 'yup'
+
 import { Router } from 'express'
 
 export const detectionRouter = Router()
 
-detectionRouter.post('/detection', (req, res) => {
-  res.status(200).send()
+const detectionSchema = yup.object({
+  sensorId: yup.string().required(),
+})
+
+detectionRouter.post('/detection', async (req, res, next) => {
+  try {
+    const { sensorId } = await detectionSchema.validate(req.body)
+
+    console.info(`sensor id: ${sensorId}`)
+
+    res.status(200).send()
+  } catch (error) {
+    next(error)
+  }
 })
