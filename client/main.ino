b2472d22ad66626842ebbf3cd428606b4791d925
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-04-24 00:19:41 -0300

    Client

--- client/main.ino
+++ client/main.ino
@@ -6,7 +6,7 @@ String ssid = "";
 String password = "";
 
 String host = "192.168.15.35";
-String port = "5500";
+String port = "3000";
 
 void awaitResponse(String stopFlag)
 {
@@ -46,13 +46,68 @@ void awaitResponse(String stopFlag)
   }
 }
 
-// void printResponse()
-// {
-//   while (esp8266.available())
-//   {
-//     Serial.println(esp8266.readStringUntil('\n'));
-//   }
-// }
+void printResponse()
+{
+  while (esp8266.available())
+  {
+    Serial.println(esp8266.readStringUntil('\n'));
+  }
+}
+
+void setupConnection()
+{
+  // WiFi connection setup.
+
+  esp8266.println("AT");
+  awaitResponse("OK");
+  Serial.println("======== 1 - AT DONE");
+
+  esp8266.println("AT+CWMODE=1");
+  awaitResponse("OK");
+  Serial.println("======== 2 - CWMODE DONE");
+
+  esp8266.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"");
+  awaitResponse("WIFI GOT IP");
+  Serial.println("======== 3 - CWJAP DONE");
+}
+
+void sendRequest()
+{
+  esp8266.println("AT+CIPMUX=1");
+  awaitResponse("OK");
+
+  esp8266.println("AT+CIPSTART=4,\"TCP\",\"" + host + "\"," + port);
+  delay(100);
+  printResponse();
+
+  String cmd = "POST /api/v1/event/detection HTTP/1.1";
+  esp8266.println("AT+CIPSEND=4," + String(cmd.length() + 4));
+  delay(100);
+
+  esp8266.println(cmd);
+  delay(100);
+  esp8266.println("");
+
+  // while (true)
+  // {
+  //   if (esp8266.available())
+  //   {
+  //     String a = "";
+
+  //     while (esp8266.available())
+  //     {
+  //       char j = esp8266.read();
+
+  //       a += j;
+  //     }
+
+  //     Serial.println("================");
+  //     Serial.println(a);
+  //   }
+
+  //   delay(10);
+  // }
+}
 
 void setup()
 {
@@ -62,85 +117,29 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(2, INPUT);
 
-  Serial.println("=== 0");
+  // Fix baud rate.
+  // esp8266.begin(115200);
+  // Serial.println("=== SETUP");
+  // esp8266.println("AT+UART_DEF=9600,8,1,0,0");
+  // Serial.println("=== SETUP DONE");
 
-  // WiFi connection setup.
-
-  esp8266.println("AT");
-  awaitResponse("OK");
-
-  Serial.println("=== 1");
-
-  esp8266.println("AT+CWMODE=1");
-  awaitResponse("OK");
-
-  Serial.println("=== 2");
-
-  esp8266.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"");
-  awaitResponse("WIFI GOT IP");
-
-  Serial.println("=== 3");
-
-  // WiFi connection test.
-
-  esp8266.println("AT+CIPMUX=1");
-  awaitResponse("OK");
-
-  Serial.println("=== 4");
-
-  esp8266.println("AT+CIPSTART=\"TCP\",\"" + host + "\"," + port);
-  awaitResponse("OK");
-
-  Serial.println("=== 5");
-
-  // String cmd = "GET /test.html HTTP/1.1";
-  // esp8266.println("AT+CIPSEND=4," + String(cmd.length() + 4));
-  // delay(1000);
-
-  // Serial.println("=== 6");
-
-  // esp8266.println(cmd);
-  // delay(1000);
-  // esp8266.println("");
-
-  // Serial.println("=== 7");
+  setupConnection();
 }
 
 void loop()
 {
-  // int in = digitalRead(2);
+  int in = digitalRead(2);
 
-  // digitalWrite(LED_BUILTIN, in);
+  digitalWrite(LED_BUILTIN, in);
 
-  // if (in == 1)
-  // {
-  //   Serial.write("detected");
+  if (in == 1)
+  {
+    Serial.println(">>> triggering detection");
 
-  //   delay(5000);
-  // }
+    sendRequest();
 
-  // if (Serial.available() > 0)
-  // {
-  //   String command = Serial.readStringUntil('\n');
-  //   Serial.println("Command Sent: " + command);
-  //   Serial.println();
-  //   esp8266.println(command);
-  // }
+    delay(1000);
+  }
 
-  // int responseCounter = 0;
-
-  // if (esp8266.available() > 0)
-  // {
-  //   while (esp8266.available() > 0)
-  //   {
-  //     if (responseCounter == 0)
-  //     {
-  //       Serial.println("Response Received:");
-  //     }
-
-  //     String response = esp8266.readStringUntil('\n');
-  //     Serial.println(response);
-  //     responseCounter++;
-  //   }
-  // }
+  delay(100);
 }
